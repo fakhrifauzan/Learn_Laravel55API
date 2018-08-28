d1dadd352f15395b6dbeb7171c123d1833fd88df
fakhri.fauzan <fakhri.fauzan@midtrans.com>
2018-08-28 13:56:40 +0700

    add article resources

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('articles', 'ArticleController');
 Route::get('/products/{id}', 'ProductController@show');
